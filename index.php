b219d3e95db2c0622ebc68a4db53093aa217fb38
WillsPrettyLxzy <109177082+WillsPrettyLxzy@users.noreply.github.com>
2024-12-08 18:25:18 -0500

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Include the main server file to handle requests.
-require_once 'restaurant_server.php';
-
-// Create an instance of the RestaurantPortal class and handle the request.
-$portal = new RestaurantPortal();
-$portal->handleRequest();
